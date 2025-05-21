af36bc9ecddda44959e78503632d434f22eb9b93
astroNmoh <moh_la_cs@hotmail.com>
2025-05-21 20:33:32 -0300

    Update SlotMacineAnimator.cs

--- Assets/Scripts/SlotMacineAnimator.cs
+++ Assets/Scripts/SlotMacineAnimator.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System;
 using UnityEngine.UI;
 using System.Collections;
+
 public class SlotMachineAnimator : MonoBehaviour
 {
 	[SerializeField] private SlotMachineController slotController; //We could setup an interface to decouple more
 	[SerializeField] private GameObject[] reelBGs = new GameObject[5]; //Create this dinamically if needed, just keeping it simple here
 	private Dictionary<Reels, Sprite> reelSprites = new Dictionary<Reels, Sprite>();
+	private Vector3[] lastColumnReelPos = new Vector3[5];
 	
 	
 	public void Init()
@@ -15,11 +17,8 @@ public class SlotMachineAnimator : MonoBehaviour
 		foreach (Reels reel in Enum.GetValues(typeof(Reels)))
 		{
 			int reelIndex = (int)reel + 1;
-			//Debug.Log("address: " + ("Figures/" + reelIndex).ToString());
 			Texture2D texture = Resources.Load<Texture2D>("Figures/" + reelIndex);
-			//Debug.Log(texture);
 			Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
-
 			reelSprites.Add(reel, sprite);
 		}
 
@@ -47,7 +46,14 @@ public class SlotMachineAnimator : MonoBehaviour
 	{
 		for (int i = 0; i < reelBGs.Length; i++)
 		{
-      StartCoroutine(MoveReelsBottomToTop(reelBGs[i], duration));
+      //lastColumnReelPos[i] = new Vector3(
+    //  reelBGs[i].transform.GetChild(reelBGs[i].transform.childCount - 1).GetComponent<RectTransform>().localPosition.x,
+   // reelBGs[i].transform.GetChild(reelBGs[i].transform.childCount - 1).GetComponent<RectTransform>().localPosition.y,
+   // reelBGs[i].transform.GetChild(reelBGs[i].transform.childCount - 1).GetComponent<RectTransform>().localPosition.z
+//);
+
+    lastColumnReelPos[i] = reelBGs[i].transform.GetChild(reelBGs[i].transform.childCount - 1).GetComponent<RectTransform>().localPosition;
+      StartCoroutine(MoveReelsBottomToTop(reelBGs[i], duration, lastColumnReelPos[i]));
 			yield return new WaitForSeconds(columnInterval);
 		}
 	}
@@ -62,24 +68,24 @@ public class SlotMachineAnimator : MonoBehaviour
 			Debug.LogWarning($"No sprite found for {reelType}");
 		}
 	}
-	private IEnumerator MoveReelsBottomToTop(GameObject column, float duration)
+	private IEnumerator MoveReelsBottomToTop(GameObject column, float duration, Vector3 columnLastPos)
 	{
-		float topPositionY = column.transform.GetChild(0).GetComponent<RectTransform>().localPosition.y;
-		Debug.Log("Column nro " + column.name + " with top reel at y: " + topPositionY);
-		RectTransform lastChildRTrans = column.transform.GetChild(column.transform.childCount - 1).GetComponent<RectTransform>();
-		Transform firstChild = column.transform.GetChild(0);
+		//float topPositionY = column.transform.GetChild(0).GetComponent<RectTransform>().localPosition.y;
+		//Debug.Log("Column nro " + column.name + " with top reel at y: " + topPositionY);
+		//RectTransform lastChildRTrans = column.transform.GetChild(column.transform.childCount - 1).GetComponent<RectTransform>();
+		//Transform firstChild = column.transform.GetChild(0);
 
 		// Move all elements down smoothly
 		foreach (Transform reel in column.transform)
 		{
 			//float moveDistance = column.transform.GetChild(column.transform.childCount -1).GetComponent<RectTransform>().localPosition.y;
 			float speedRotation = -1000;
-			StartCoroutine(MoveUIReel(reel, speedRotation, duration, lastChildRTrans.localPosition));
+			StartCoroutine(MoveUIReel(reel, speedRotation, duration, columnLastPos));
 		}
 
 		// Wait for the movement to complete
 		yield return new WaitForSeconds(duration);
-
+		/* CLEAN tHIS
     List<Transform> reelsToMove = new List<Transform>();
     foreach (Transform reel in column.transform)
 		{
@@ -90,7 +96,7 @@ public class SlotMachineAnimator : MonoBehaviour
 		{
       reel.SetSiblingIndex(column.transform.childCount - 1);
     }
-
+		*/
   }
   private IEnumerator MoveUIReel(Transform reel, float targetY, float duration, Vector3 lastColumnPos)
 	{
@@ -111,11 +117,16 @@ public class SlotMachineAnimator : MonoBehaviour
 				float lastReelPos = reel.GetComponent<RectTransform>().localPosition.y;
         reel.GetComponent<RectTransform>().localPosition = lastColumnPos;
 
-        //Debug.Log("moving reel to last post" + reel.GetComponent<RectTransform>().localPosition);
-
+				Debug.Log("TARGET Y: " + target.y);
+				//Debug.Log("moving reel to last post" + reel.GetComponent<RectTransform>().localPosition);
+				Debug.Log("startPos.Y: " + startPos.y);
+				Debug.Log("lastReelPos before moved down: " + lastReelPos);
+				Debug.Log("RemainingDistance: " + (target.y - lastReelPos));
+				
         // Update start position so Lerp continues from the new location
-        float remainingDistance = target.y - lastReelPos - startPos.y;
-        target = new Vector3(startPos.x, startPos.y - remainingDistance, startPos.z);
+        float remainingDistance = target.y - lastReelPos;
+				Debug.Log("ReaminingDistance: " + remainingDistance);
+        target = new Vector3(lastColumnPos.x, lastColumnPos.y + remainingDistance, lastColumnPos.z);
 				//Debug.Log("New targetY: " + target.y);
         startPos = lastColumnPos;
       }
